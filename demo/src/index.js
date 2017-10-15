fd79d3a6df1e99d6a6d5b0977b57ef6f2d085903
Tomas Ramirez <tomas.ramirezsarduy@thetrainline.com>
2017-10-15 23:23:34 +0100

    Add more examples and custom styles for demos

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { DebounceInput } from 'react-debounce-input';
 import ProgressTracker from '../../src/index';
+import styles from './index.styles';
 
 class Demo extends Component {
   constructor(props) {
@@ -18,9 +19,37 @@ class Demo extends Component {
 
   render() {
     return (
-      <div>
+      <div style={styles.wrapper}>
         <h1>react-circular-progress Demo</h1>
-        <ProgressTracker progress={this.state.progress} />
+        <div style={styles.progressContainer}>
+          <ProgressTracker progress={this.state.progress} />
+        </div>
+        <div style={styles.progressContainer}>
+        <ProgressTracker 
+          progress={this.state.progress} 
+          borderColor='#2596b8'
+          textColor='#2596b8'
+          backgroundColor='#e9f0f3'
+        />
+        </div>
+        <div style={styles.progressContainer}>
+        <ProgressTracker 
+          progress={70} 
+          borderColor='#3b1154'
+          textColor='white'
+          backgroundColor='#8027b7'
+          showPercent={true}
+        />
+        </div>
+        <div style={styles.progressContainer}>
+        <ProgressTracker 
+          progress={90} 
+          borderColor='#3b1154'
+          textColor='#f5f5f5'
+          backgroundColor='#8027b7'
+          showNumber={false}
+        />
+        </div>
         <h2>Update progress value</h2>
         <DebounceInput
           type="text"
---
+++ demo/src/index.styles.js
@@ -0,0 +1,13 @@
+export default {
+  wrapper: {
+    fontFamily: 'sans-serif'
+  },
+  title: {
+    fontSize: '42px',
+    color: '#72b924',
+  },
+  progressContainer: {
+    display: 'inline-block',
+    margin: '10px'
+  }
+};
